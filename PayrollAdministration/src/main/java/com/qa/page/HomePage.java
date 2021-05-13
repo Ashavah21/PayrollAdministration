bfa0ebb378ba7aa1696f9ce79cca498bd3c84667
Ashavah21 <81513153+Ashavah21@users.noreply.github.com>
2021-05-13 16:07:01 +0530

    Delete PayrollAdministration directory

--- PayrollAdministration/src/main/java/com/qa/page/HomePage.java
+++
@@ -1,5 +0,0 @@
-package com.qa.page;
-
-public class HomePage {
-
-}
--- PayrollAdministration/src/test/java/com/qa/test/HomeTest.java
+++
@@ -1,9 +0,0 @@
-package com.qa.test;
-
-import org.testng.annotations.Test;
-
-public class HomeTest {
-  @Test
-  public void f() {
-  }
-}
